6f197650978c38368cb5345bd17d64c03fd0413b
Samuel <smelton@sw-construction.com>
2022-07-25 16:00:44 -0500

    added remove local admin

---
+++ RemoveLocalAdmin.ps1
@@ -0,0 +1,15 @@
+
+
+$searchBase = 'OU=Computers,OU=_SWC,DC=sw-construction,DC=com'
+$allComputers = Get-ADComputer -SearchBase $searchBase -SearchScope Subtree -Properties 'OperatingSystem'-Filter *
+$credential = Get-Credential
+
+
+ForEach ($computer in $allComputers) {
+    $computerIsEnabled = $computer.Enabled
+    $computerIsWindows10Pro = ($computer.OperatingSystem -eq 'Windows 10 Pro')
+    if ($computerIsEnabled -and $computerIsWindows10Pro) {
+        Enter-PSSession -ComputerName $computer.Name -Credential $credential
+        Remove-LocalUser -Name 'admin' 
+    }
+}
