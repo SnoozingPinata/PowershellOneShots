e1b5f79bc138ba2ce754dc282547dc4069d9a8af
Samuel <smelton@sw-construction.com>
2022-08-01 09:50:05 -0500

    added mail rule finder script

---
+++ MailRuleFinder.ps1
@@ -0,0 +1,33 @@
+
+# Change these varibles for your environment.
+$adminUserName = ''
+$outputFilePath = "C:\365Reports\365MailRulesReport.csv"
+
+# Connecting to Exchange Online. Need to change this if you're using a different service. (like on prem)
+Connect-EXOPSSession -UserPrincipalName $adminUserName
+
+# Adding role to account so you can run this report.
+Add-RoleGroupMember -Identity "Discovery Management" -Member $adminUserName
+
+# Getting all of the mailboxes besides the 'Admin' one.
+# Change the alias string to whatever your admin's alias is.
+$AdminMailboxAlias = 'Admin'
+$allMailboxes = Get-Mailbox -ResultSize unlimited -Filter "Alias -ne '$($AdminMailboxAlias)'"
+
+# Changing permissions and turning on auditing on the mailboxes.
+foreach ($mailbox in $allMailboxes) {
+    # if the mailbox is a user mailbox (opposed to a shared mailbox), turns on auditing
+    if ($mailbox.RecipientTypeDetails -eq 'UserMailbox') {
+        Set-Mailbox -Identity $mailbox.DistinguishedName -AuditEnabled $true
+    }
+    Add-MailboxPermission -Identity $mailbox.DistinguishedName -User $adminUserName -AccessRights FullAccess -InheritanceType all -AutoMapping $False
+}
+
+# Gets the mailbox rule and exports to csv.
+# Could make this better by adding this bit into the loop above.
+Get-Mailbox | ForEach-Object { 
+    Get-InboxRule -Mailbox $_.DistinguishedName 
+} | Export-Csv $outputFilePath
+
+# Closes out of all the active powershell sessions. Change this if you might have other sessions open simultaneously.
+Get-PSSession | Remove-PSSession
--- README.md
+++ README.md
@@ -2,10 +2,15 @@
 Random useful scripts I created for a specific one-time tasks. 
 
 ChangeADNumberFormat.ps1
- - This script replaces a character within all phone number fields for every user in Active Directory.
+  - This script replaces a character within all phone number fields for every user in Active Directory.
 
 RemoveLocalAdmin.ps1
- - This script connects to each Windows 10 computer in the domain and removes the local admin account named 'admin'.
+  - This script connects to each Windows 10 computer in the domain and removes the local admin account named 'admin'.
 
- CheckAccounts.ps1
+CheckAccounts.ps1
   - This script takes a list of account names and checks Active Directory to see if they exist and if they are enabled.
+
+MailRuleFinder.ps1
+  - This script connects to exchange online, adds a role to the target account, adds permissions to all mailboxes, gets all mail rules on all accounts, and exports the data to a CSV. 
+  - The main objective is to scan mail rules for signs of intruders.
+  - Not sure if this works, but it should be close. (Made it for someone else and didn't test in my environment.)
